2a8b0afc37ed391a5081e094408641ab6d22ee22
ikhlef meriem <m.ikhlef@esi-sba.dz>
2024-09-10 21:00:12 +0100

    second try

--- library.js
+++ library.js
@@ -1,29 +1,3 @@
-const library=[]
-function book(title,author,pages,isread){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.isread=isread;
-}
-function addbook(){
-    library.push(book);
-function displaybooks() {
-        const library=document.getElementById('library')
-        //library.innerHTML = ''; // Clear previous display
-
-    myLibrary.forEach((book, index) => {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    bookCard.innerHTML = `
-      <h2>${book.title}</h2>
-      <p>Author: ${book.author}</p>
-      <p>Pages: ${book.pages}</p>
-      <p>Status: ${book.isRead ? 'Read' : 'Not Read'}</p>
-      <button onclick="removeBook(${index})">Remove</button>
-      <button onclick="toggleRead(${index})">${book.isRead ? 'Unread' : 'Read'}</button>
-    `;
-    library.appendChild(bookCard);
-  });
-}
-};
+const dialog = document.querySelector("dialog")
+dialog.showModal()
  
